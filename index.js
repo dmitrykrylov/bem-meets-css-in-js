f1ebc3688a082c130cae456b23d10c9e8fdbefe3
dkrylov <dimakrylov@spark-mail.ru>
2019-04-22 14:13:15 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import "impress.js";
+import Prism from "prismjs";
+import "prismjs/themes/prism-okaidia.css";
+import "prismjs/components/prism-jsx.min.js";
+
+impress().init();
+
+hljs.initHighlightingOnLoad();
